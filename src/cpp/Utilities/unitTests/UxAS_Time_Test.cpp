a3423d07a0667f98f98639ab3125d82f69c89966
Phil Williams <charles.williams@udri.udayton.edu>
2023-05-19 16:45:16 -0400

    Interim commit of initial testing code.

---
+++ src/cpp/Utilities/unitTests/UxAS_Time_Test.cpp
@@ -0,0 +1,12 @@
+#include "UxAS_Time.h"
+#include "UxAS_Time_Test.h"
+
+namespace uxas {
+namespace common {
+
+TEST_F(UxAS_Time_Test, TimeTest) {
+    ASSERT_TRUE(true);
+}
+
+}
+}
---
+++ src/cpp/Utilities/unitTests/UxAS_Time_Test.h
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+
+namespace uxas {
+namespace common {
+
+class UxAS_Time_Test : public ::testing::Test {
+public:
+    // Override google-test blank SetUp method
+    void SetUp() override {};
+
+    // Override google-test blank TearDown method
+    void TearDown() override {};
+};
+
+}
+}
